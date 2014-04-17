0880e96528b7c10bd81a2b9ee7008aa6666c4f19
Roey Chasman <rchasman@email.arizona.edu>
2014-04-17 10:40:00 -0700

    Initialize reveal.js

--- src/scripts/drawing.pde
+++ src/scripts/drawing.pde
@@ -4,9 +4,6 @@ void setup() {
   size(300, 300);
   background(255);
   a = new Actor(8, 0.5);
-  //stroke(255);
-  //ellipse(50, 50, 25, 25);
-  //println("hi");
 }
 
 void draw() {
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,34 @@
-bespoke.from('article', {
-  keys: true,
-  touch: true,
-  bullets: 'li, .bullet',
-  scale: true,
-  hash: true,
-  progress: true,
-  forms: true
+Reveal.initialize({
+    controls: false,
+    progress: true,
+    slideNumber: false,
+    history: false,
+    keyboard: true,
+    overview: true,
+    center: true,
+    touch: true,
+    loop: false,
+    rtl: false,
+    fragments: true,
+    embedded: false,
+    autoSlide: 0,
+    autoSlideStoppable: true,
+    mouseWheel: false,
+    hideAddressBar: true,
+    previewLinks: false,
+    transition: 'linear',
+    transitionSpeed: 'fast',
+    backgroundTransition: 'default',
+    viewDistance: 3,
+    parallaxBackgroundImage: '/styles/bg.jpg'
 });
 
 var canvas = document.getElementById("sketch");
 // attaching the sketchProc function to the canvas
 var p;
 var pauser = true;
+
+Processing.logger = console;
 
 function switcher(on) {
   if (!p) {
